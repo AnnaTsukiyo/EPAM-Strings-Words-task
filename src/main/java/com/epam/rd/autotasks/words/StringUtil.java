0f02cfca17a479e44cd3d06f0cc6468184a9f993
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-10 14:19:20 +0200

    done

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -1,22 +1,128 @@
 package com.epam.rd.autotasks.words;
 
 import java.util.Arrays;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtil {
     public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-        throw new UnsupportedOperationException();
+        if (sample != null  && words != null) {
+            sample = sample.strip();
+            int count = 0;
+            for (String word: words) {
+                if (word.trim().equalsIgnoreCase(sample)) {
+                    count += 1;
+                }
+            }
+            return count;
+        } else {
+            return 0;
+        }
+
     }
 
     public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+        if (text != null && !text.equals("")) {
+            Pattern p1 = Pattern.compile("\\W*");
+            Matcher m1 = p1.matcher(text);
+            if (!m1.matches()) {
+                Pattern p2 = Pattern.compile("\\A\\W*");
+                Matcher m2 = p2.matcher(text);
+                text = m2.replaceFirst("");
+                String[] result = text.split("\\W+");
+                if (result.length > 0) {
+                    return result;
+                }
+            }
+        }
+        return null;
     }
 
     public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+        if (path == null || path.equals("")) {
+            return null;
+        }
+        if (path.lastIndexOf('~') > 0) {
+            return null;
+        }
+        if (path.lastIndexOf("C:") > 1) {
+            return null;
+        }
+        if ((path.indexOf('\\') != -1) && path.indexOf('~') != -1) {
+            return null;
+        }
+        if ((path.indexOf('\\') != -1) && path.indexOf('/') != -1) {
+            return null;
+        }
+        if ((path.indexOf("C:") != -1) && path.indexOf('/') != -1) {
+            return null;
+        }
+        if ((path.indexOf("C:") != -1) && path.indexOf('~') != -1) {
+            return null;
+        }
+
+        if (path.lastIndexOf("C:") > 1) {
+            return null;
+        }
+        if ((path.indexOf("[//+]") != -1)) {
+            return null;
+        }
+        if (toWin) {
+            int i = path.lastIndexOf('~');
+            int i1 = path.lastIndexOf('\\');
+            int i2 = path.lastIndexOf("C:");
+
+            if (i < 1 && i1 == -1 && i2 == -1) {
+                if (path.charAt(0) == '~' && path.length() != 1) {
+                    path = path.replaceFirst("~/", "C:\\\\User\\\\");
+
+                } else if (path.charAt(0) == '~' && path.length() == 1) {
+                    path = path.replaceFirst("~", "C:\\\\User");
+                } else if (path.charAt(0) == '/') {
+                    path = path.replaceFirst("/", "C:\\\\");
+                }
+                path = path.replaceAll("/", "\\\\");
+
+                return path;
+            }
+        } else {
+            int i = path.lastIndexOf('C');
+            int i1 = path.lastIndexOf('/');
+            int i2 = path.lastIndexOf("~");
+            if (i < 1 && i1 == -1 && i2 == -1) {
+                if (path.indexOf("C:\\User") != -1) {
+                    path = path.replaceFirst("C:\\\\User", "~");
+                } else if (path.charAt(0) == 'C') {
+                    path = path.replaceFirst("C:\\\\", "/");
+                } else if (path.charAt(0) == '/') {
+                    path = path.replaceFirst("/", "C:\\\\");
+                } else {
+                }
+                path = path.replaceAll("\\\\", "/");
+
+                return path;
+            }
+        }
+        return path;
     }
 
+
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+        boolean notEmpty = false;
+        if (words != null && words.length > 0) {
+            StringJoiner joiner = new StringJoiner(", ", "[", "]");
+            for (String word: words) {
+                if (!word.equals("")){
+                    notEmpty = true;
+                    joiner.add(word);
+                }
+            }
+            if (notEmpty) {
+                return joiner.toString();
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
